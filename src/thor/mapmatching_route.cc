eebc9a621383e9d375a65bb9e440456aae9bd6cd
Duane Gearhart <duane@mapzen.com>
2016-10-13 14:26:41 -0400

    Added logic to make sure that the origin and destination candidates have valid state - thanks @ptpt and @kevinkreiser Also, added exception handling for invalid path

--- src/thor/mapmatching_route.cc
+++ src/thor/mapmatching_route.cc
@@ -1,6 +1,8 @@
 #include <vector>
 #include <algorithm>
+#include <exception>
 #include <valhalla/midgard/logging.h>
+#include <valhalla/baldr/errorcode_util.h>
 
 #include "thor/mapmatching_route.h"
 
@@ -29,42 +31,49 @@ std::vector<PathInfo> MapMatchingRoute::FormPath(
   const DirectedEdge* directededge;
   EdgeLabel pred;
 
-  for (const auto& edge_segment : ConstructRoute(matcher->mapmatching(),
-                                                 results.begin(), results.end())) {
-    // Skip edges that are the same as the prior edge
-    if (edge_segment.edgeid == prior_edge) {
-      continue;
+  try {
+    auto edge_segments = ConstructRoute(matcher->mapmatching(), results.begin(),
+                                        results.end());
+    for (const auto& edge_segment : edge_segments) {
+      // Skip edges that are the same as the prior edge
+      if (edge_segment.edgeid == prior_edge) {
+        continue;
+      }
+
+      // Get the directed edge
+      GraphId edge_id = edge_segment.edgeid;
+      const GraphTile* tile = matcher->graphreader().GetGraphTile(edge_id);
+      directededge = tile->directededge(edge_id);
+
+      if (directededge && nodeinfo) {
+        // Get transition cost
+        elapsed_time += costing->TransitionCost(directededge, nodeinfo, pred)
+                .secs;
+
+        // Get time along the edge, handling partial distance along
+        // the first and last edge
+        elapsed_time += costing->EdgeCost(directededge, nodeinfo->density())
+                .secs * (edge_segment.target - edge_segment.source);
+      }
+
+      // Update the prior_edge and nodeinfo. TODO (protect against invalid tile)
+      prior_edge = edge_id;
+      prior_node = directededge->endnode();
+      const GraphTile* end_tile = matcher->graphreader().GetGraphTile(
+          prior_node);
+      nodeinfo = end_tile->node(prior_node);
+
+      // Create a predecessor EdgeLabel (for transition costing)
+      pred = {kInvalidLabel, edge_id, directededge, {},
+          0, 0, directededge->restrictions(),
+          directededge->opp_local_idx(), mode, 0};
+
+      // Add to the PathInfo
+      path.emplace_back(mode, elapsed_time, edge_id, 0);
     }
-
-    // Get the directed edge
-    GraphId edge_id = edge_segment.edgeid;
-    const GraphTile* tile = matcher->graphreader().GetGraphTile(edge_id);
-    directededge = tile->directededge(edge_id);
-
-    if (directededge && nodeinfo) {
-      // Get transition cost
-      elapsed_time +=
-          costing->TransitionCost(directededge, nodeinfo, pred).secs;
-
-      // Get time along the edge, handling partial distance along
-      // the first and last edge
-      elapsed_time += costing->EdgeCost(directededge, nodeinfo->density()).secs
-          * (edge_segment.target - edge_segment.source);
-    }
-
-    // Update the prior_edge and nodeinfo. TODO (protect against invalid tile)
-    prior_edge = edge_id;
-    prior_node = directededge->endnode();
-    const GraphTile* end_tile = matcher->graphreader().GetGraphTile(prior_node);
-    nodeinfo = end_tile->node(prior_node);
-
-    // Create a predecessor EdgeLabel (for transition costing)
-    pred = {kInvalidLabel, edge_id, directededge, {},
-      0, 0, directededge->restrictions(),
-      directededge->opp_local_idx(), mode, 0};
-
-    // Add to the PathInfo
-    path.emplace_back(mode, elapsed_time, edge_id, 0);
+  }
+  catch(const std::exception& e) {
+    throw valhalla_exception_t { 400, 442 };
   }
   return path;
 }
